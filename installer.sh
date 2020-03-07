5456c530351caa1b6fb6b2c64e1456e9c91017ab
Ahmed Abdulrahman <a.kasapbashi@live.com>
2020-03-07 20:36:03 +0100

    :wrench: [Installer] update bootstrap funciton, update indentation
    Signed-off-by: Ahmed Abdulrahman <a.kasapbashi@live.com>

--- installer.sh
+++ installer.sh
@@ -8,130 +8,128 @@ declare -r OAKWOOD_TOOLBOX_UTILS_URL="https://raw.githubusercontent.com/$GITHUB_
 declare -r OAKWOOD_TOOLBOX="$HOME/oakwood-macos-toolbox"
 
 download() {
-    local url="$1"
-    local output="$2"
+	local url="$1"
+	local output="$2"
 
-    if command -v "curl" &> /dev/null; then
-        curl -LsSo "$output" "$url" &> /dev/null
-        return $?
+	if command -v "curl" &> /dev/null; then
+		curl -LsSo "$output" "$url" &> /dev/null
+		return $?
 
-    elif command -v "wget" &> /dev/null; then
-        wget -qO "$output" "$url" &> /dev/null
-        return $?
-    fi
-    return 1
+	elif command -v "wget" &> /dev/null; then
+		wget -qO "$output" "$url" &> /dev/null
+		return $?
+	fi
+	return 1
 }
 
 download_utils() {
-    local tmpFile=""
-    tmpFile="$(mktemp /tmp/XXXXX)"
+	local tmpFile=""
+	tmpFile="$(mktemp /tmp/XXXXX)"
 
-    download "$TOOLBOX_UTILS_URL" "$tmpFile" \
-        && . "$tmpFile" \
-        && rm -rf "$tmpFile" \
-        && return 0
-   return 1
+	download "$TOOLBOX_UTILS_URL" "$tmpFile" \
+		&& . "$tmpFile" \
+		&& rm -rf "$tmpFile" \
+		&& return 0
+	return 1
 }
 
 on_start() {
 
-  print_main_header
-  print_info "This script will guide you through Installing Oakwood Toolbox for your macOS."
-  print_info "It won't install anything without your agreement!"
+	print_main_header
+	print_info "This script will guide you through Installing Oakwood Toolbox for your macOS."
+	print_info "It won't install anything without your agreement!"
 
-  ask_for_confirmation "Do you want to proceed with installation?"
+	ask_for_confirmation "Do you want to proceed with installation?"
 
-  if ! answer_is_yes; then
-    exit 1
-  fi
+	if ! answer_is_yes; then
+		exit 1
+	fi
 
 }
 
 install_command_line_tools() {
-  # Note:There's not need to install XCode tools on Linux
-  if [ `uname` == 'Linux' ]; then
-    return
-  fi
+	# Note: We don't need to install XCode tools on Linux
 
-  ./scripts/xcode.sh
+	if [ `uname` == 'Linux' ]; then
+		return
+	fi
 
-  finish
+	. "scripts/macos/xcode.sh"
+
+	finish
 }
 
 install_git() {
   print_info "Trying to detect installed Git..."
 
   if ! cmd_exists "git"; then
-    print_info "Seems like you don't have Git installed!"
+	print_info "Seems like you don't have Git installed!"
 
-    ask_for_confirmation "Do you agree to proceed with Git installation?"
+	ask_for_confirmation "Do you agree to proceed with Git installation?"
 
-    if ! answer_is_yes; then
-      return
-    fi
+	if ! answer_is_yes; then
+	  return
+	fi
 
-    ./scripts/macos/git.sh
+	. "scripts/macos/git.sh"
 
   else
-    print_info "You already have Git installed. Skipping..."
+	print_info "You already have Git installed. Skipping..."
   fi
 
   finish
 }
 
 install_package_manager() {
-  if [ `uname` == 'Darwin' ]; then
+	if [ `uname` == 'Darwin' ]; then
+		print_info "Trying to detect if Homebrew is installed..."
 
-    print_info "Trying to detect if Homebrew is installed..."
+		if ! cmd_exists "brew"; then
+			print_warning "Seems like you don't have Homebrew installed!"
+			print_info "Installing Homebrew...This may take a while"
 
-    if ! cmd_exists "brew"; then
-      print_warning "Seems like you don't have Homebrew installed!"
-      print_info "Installing Homebrew...This may take a while"
+			. "scripts/macos/homebrew.sh"
 
-      ./scripts/macos/homebrew.sh
-
-    else
-      print_info "You already have Homebrew installed, nothing to do here skipping ... 💨"
-    fi
-
-  fi
-
-  finish
+		else
+			print_info "You already have Homebrew installed, nothing to do here skipping ... 💨"
+		fi
+	fi
+	finish
 }
 
 clone_oakwood_macos_toolbox_repo() {
-  print_question "Trying to detect if Oakwwd's macOS Toolbox is installed in $OAKWOOD_TOOLBOX..."
+	print_question "Trying to detect if Oakwwd's macOS Toolbox is installed in $OAKWOOD_TOOLBOX..."
 
-  if [ ! -d $OAKWOOD_TOOLBOX ]; then
-    print_info "Seems like you don't have Oakwood's macOS Toolbox clone!"
+	if [ ! -d $OAKWOOD_TOOLBOX ]; then
+		print_info "Seems like you don't have Oakwood's macOS Toolbox clone!"
 
-    ask_for_confirmation "Do you agree to proceed with Oakwood's macOS Toolbox installation?"
+		ask_for_confirmation "Do you agree to proceed with Oakwood's macOS Toolbox installation?"
 
-    if ! answer_is_yes; then
-      return
-    fi
+		if ! answer_is_yes; then
+			return
+		fi
 
-    print_info "Cloning Oakwood's macOS Toolbox"
-    git clone --recursive "$GITHUB_REPO_URL_BASE.git" $TOOLBOX
+		print_info "Cloning Oakwood's macOS Toolbox"
+		git clone --recursive "$GITHUB_REPO_URL_BASE.git" $TOOLBOX
 
-    # Setup repo origin & mirrors
-    cd "$OAKWOOD_TOOLBOX" &&
-      git remote set-url origin git@github.com:oakwood/oakwood-macos-toolbox.git
+		# Setup repo origin & mirrors
+		cd "$OAKWOOD_TOOLBOX" &&
+			git remote set-url origin git@github.com:oakwood/oakwood-macos-toolbox.git
 
-  else
-    print_info "You already have Oakwood's macOS Toolbox installed. Skipping..."
-    print_info "Pulling latest update..."
+	else
+		print_info "You already have Oakwood's macOS Toolbox installed. Skipping..."
+		print_info "Pulling latest update..."
 
-    cd "$OAKWOOD_TOOLBOX" &&
-      git stash -u &&
-      git checkout master &&
-      git reset --hard origin/master &&
-      git submodule update --init --recursive &&
-      git checkout - &&
-      git stash pop
-  fi
+		cd "$OAKWOOD_TOOLBOX" &&
+			git stash -u &&
+			git checkout master &&
+			git reset --hard origin/master &&
+			git submodule update --init --recursive &&
+			git checkout - &&
+			git stash pop
+	fi
 
-  finish
+	finish
 }
 
 bootstrap_os() {
@@ -143,8 +141,8 @@ bootstrap_os() {
 		fi
 
 		cd "$OAKWOOD_TOOLBOX" &&
-			. "./scripts/macos/bootstrap.sh" &&
-			. "./scripts/macos/nodejs.sh"
+			. "scripts/macos/bootstrap.sh" &&
+			. "scripts/macos/nodejs.sh"
 
 	else
 		print_warning "Make sure clone Oakwood's macOS Toolbox repo. Skipping ... 💨!"
@@ -154,23 +152,26 @@ bootstrap_os() {
 }
 
 main() {
-    # Ensure that the following actions are made relative to this file's path.
-    cd "$(dirname "${BASH_SOURCE[0]}")" \
-        || exit 1
+	# Ensure that the following actions are made relative to this file's path.
+	cd "$(dirname "${BASH_SOURCE[0]}")" \
+		|| exit 1
 
-    # Load utils
-    if [ -x "scripts/utils.sh" ]; then
-        . "scripts/utils.sh" || exit 1
-    else
-        download_utils || exit 1
-    fi
+	# Load utils
+	if [ -x "scripts/utils.sh" ]; then
+		. "scripts/utils.sh" || exit 1
+	else
+		download_utils || exit 1
+	fi
 
-    on_start
+	on_start
 
-    install_command_line_tools
-    install_package_manager
+	install_command_line_tools
+	install_git
+	install_package_manager
+	clone_oakwood_macos_toolbox_repo
+	bootstrap_os
 
-    on_finish
+	on_finish
 }
 
 main
