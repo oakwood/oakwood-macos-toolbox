8b0783a644846e70e9429223384b6ad8dd6921af
Ahmed Abdulrahman <a.kasapbashi@live.com>
2020-03-07 15:21:54 +0100

    :sparkles: [Utils] adds more utils helper: execute, set_trap, show_spinner, kill_all_subprocesses
    Signed-off-by: Ahmed Abdulrahman <a.kasapbashi@live.com>

--- scripts/utils.sh
+++ scripts/utils.sh
@@ -149,3 +149,84 @@ answer_is_yes() {
         && return 0 \
         || return 1
 }
+
+kill_all_subprocesses() {
+    local i=""
+
+    for i in $(jobs -p); do
+        kill "$i"
+        wait "$i" &> /dev/null
+    done
+}
+
+set_trap() {
+    trap -p "$1" | grep "$2" &> /dev/null \
+        || trap '$2' "$1"
+}
+
+execute() {
+    local -r CMDS="$1"
+    local -r MSG="${2:-$1}"
+    local -r TMP_FILE="$(mktemp /tmp/XXXXX)"
+
+    local exitCode=0
+    local cmdsPID=""
+
+    # End current process and all its subprocesses.
+    set_trap "EXIT" "kill_all_subprocesses"
+
+    # Execute commands in background
+    eval "$CMDS" \
+        &> /dev/null \
+        2> "$TMP_FILE" &
+
+    cmdsPID=$!
+
+    # Show a spinner if the commands require more time to complete.
+    show_spinner "$cmdsPID" "$CMDS" "$MSG"
+
+    # Wait for the commands to no longer be executing in the background, and then get their exit code.
+    wait "$cmdsPID" &> /dev/null
+    exitCode=$?
+
+    # Print output based on what happened.
+    print_result $exitCode "$MSG"
+
+    if [ $exitCode -ne 0 ]; then
+        print_error_stream < "$TMP_FILE"
+    fi
+
+    rm -rf "$TMP_FILE"
+    return $exitCode
+}
+
+show_spinner() {
+    local -r FRAMES='/-\|'
+    # shellcheck disable=SC2034
+    local -r NUMBER_OR_FRAMES=${#FRAMES}
+
+    local -r CMDS="$2"
+    local -r MSG="$3"
+    local -r PID="$1"
+    local i=0
+    local frameText=""
+
+    printf "\n\n\n"
+    tput cuu 3
+    tput sc
+
+    # Display spinner while the commands are being executed.
+    while kill -0 "$PID" &>/dev/null; do
+        frameText="   [${FRAMES:i++%NUMBER_OR_FRAMES:1}] $MSG"
+        # Print frame text.
+        if [ "$TRAVIS" != "true" ]; then
+            printf "%s\n" "$frameText"
+        else
+            printf "%s" "$frameText"
+        fi
+
+        sleep 0.2
+        # Clear frame text.
+        tput rc
+    done
+}
