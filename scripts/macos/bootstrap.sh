5083eea4a8b164e2eb3174733206e5c6eec93a51
Ahmed Abdulrahman <a.kasapbashi@live.com>
2020-03-08 17:36:32 +0100

    :sparkles: [macos/bootstrap] update brew formules
    Signed-off-by: Ahmed Abdulrahman <a.kasapbashi@live.com>

--- scripts/macos/bootstrap.sh
+++ scripts/macos/bootstrap.sh
@@ -18,62 +18,65 @@ on_start() {
 
 
 main() {
-  # Start
-  on_start
+	# Start
+	on_start
 
-  print_in_purple "\n • Installing macOS Essential Apps and CLI Utils\n\n"
+	print_in_purple "\n • Installing macOS Essential Apps and CLI Utils\n\n"
+	brew_update                              # Make sure we’re using the latest Homebrew.
+	brew_upgrade                             # Upgrade any already-installed formulae.
 
+	brew_tap "homebrew/cask-versions"
+	brew_tap "homebrew/bundle"
+	brew_tap "homebrew/services"
+	brew_tap "tavianator/tap"
 
-  brew_update                              # Make sure we’re using the latest Homebrew.
-  brew_upgrade                             # Upgrade any already-installed formulae.
+	print_in_purple "\n   Core Utils\n"
+	brew_install "Openssl" "openssl"
+	brew_install "Python" "python"
+	brew_install "Yarn" "yarn"
 
-  brew_tap "homebrew/cask-versions"
-  brew_tap "homebrew/bundle"
-  brew_tap "homebrew/services"
-  brew_tap "tavianator/tap"
+	# Utilities of the GNU system
+	brew_install "Coreutils" "coreutils"      # GNU File Shell and Text utilities
+	brew_install "Findutils" "findutils"      # Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
+	brew_install "Moreutils" "moreutils"      # Install some other useful utilities like `sponge`.
 
-  print_in_purple "\n   Core\n"
-  brew_install "Openssl" "openssl"
-  brew_install "Python" "python"
+	brew_install "CURL" "curl"
+	brew_install "JSON processor" "jq"
+	brew_install "Grep" "grep"
+	brew_install "Pyenv" "pyenv"
+	brew_install "Pip Env" "pipenv"
+	brew_install "Ansible" "ansible"
 
-  # Utilities of the GNU system
-  brew_install "Coreutils" "coreutils"      # GNU File Shell and Text utilities
-  brew_install "Findutils" "findutils"      # Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
-  brew_install "Moreutils" "moreutils"      # Install some other useful utilities like `sponge`.
+	# A simple command line interface for the Mac App Store
+	brew_install "MAS -> Mac App Store command line interface" "mas"
 
-  brew_install "CURL" "curl"
-  brew_install "JSON processor" "jq"
-  brew_install "Grep" "grep"
-  brew_install "Pyenv" "pyenv"
-  brew_install "Pip Env" "pipenv"
-  brew_install "Ansible" "ansible"
+	print_in_purple "\n   Browsers\n"
+	brew_install "Chrome" "google-chrome" "homebrew/cask" "cask"
+	brew_install "Chrome Canary" "google-chrome-canary" "homebrew/cask-versions" "cask"
+	brew_install "Firefox" "firefox" "homebrew/cask" "cask"
+	brew_install "Firefox Developer" "firefox-developer-edition" "homebrew/cask-versions" "cask"
 
-  print_in_purple "\n   Languages\n"
-  brew_install "Node" "node"
-  brew_install "Yarn" "yarn"
+	print_in_purple "\n   Web Development\n"
+	brew_install "iTerm2" "iterm2" "homebrew/cask" "cask"
+	brew_install "Virtualbox" "virtualbox" "homebrew/cask" "cask"
+	brew_install "Vagrant" "vagrant" "homebrew/cask" "cask"
+	brew_install "Vagrant Manager" "vagrant-manager" "homebrew/cask" "cask"
+	brew_install "Insomnia" "insomnia" "homebrew/cask" "cask"
+	brew_install "Sequel Pro" "sequel-pro" "homebrew/cask" "cask"
+	brew_install "VSCode" "visual-studio-code" "homebrew/cask" "cask"
 
-  # A simple command line interface for the Mac App Store
-  brew_install "MAS -> Mac App Store command line interface" "mas"
+	print_in_purple "\n   Design\n"
+	brew_install "Figma" "figma" "homebrew/cask" "cask"
 
-  print_in_purple "\n   Desktop applications\n"
-  brew_install "Chrome" "google-chrome" "homebrew/cask" "cask"
-  brew_install "Firefox" "firefox" "homebrew/cask" "cask"
-  brew_install "iTerm2" "iterm2" "homebrew/cask" "cask"
-  brew_install "Virtualbox" "virtualbox" "homebrew/cask" "cask"
-  brew_install "Vagrant" "vagrant" "homebrew/cask" "cask"
-  brew_install "Vagrant Manager" "vagrant-manager" "homebrew/cask" "cask"
+	print_in_purple "\n  Optional Apps\n"
+	brew_install "ImageOptim" "imageoptim" "homebrew/cask" "cask"
+	brew_install "Alfred" "alfred" "homebrew/cask" "cask"
+	brew_install "Spectacle" "spectacle" "homebrew/cask" "cask"
 
-  print_in_purple "\n   Installing Optional Applications...\n"
-  brew_install "Sequel Pro" "sequel-pro" "homebrew/cask" "cask"
-  brew_install "ImageOptim" "imageoptim" "homebrew/cask" "cask"
-  brew_install "Alfred" "alfred" "homebrew/cask" "cask"
-  brew_install "Spectacle" "spectacle" "homebrew/cask" "cask"
-  brew_install "VSCode" "visual-studio-code" "homebrew/cask" "cask"
+	# Slack
+	mas install 803453959
 
-  # Slack
-  mas install 803453959
-
-  finish
+	finish
 }
 
 main
